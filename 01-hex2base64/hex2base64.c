cc496d24edd481d859c803d95d9949b65ccf1ea8
Edward Seymour <dev@edwardseymour.me>
2017-10-09 21:30:56 -0600

    Finish main implementation

--- 01-hex2base64/hex2base64.c
+++ 01-hex2base64/hex2base64.c
@@ -2,11 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "encoding.h"
+
 int main(int argc, char **argv) {
   if (argc == 1) {
     fprintf(stderr, "usage: %s hex_string\n", strrchr(argv[0], '/') + 1);
     return EXIT_FAILURE;
   }
+
+  ByteString *rawBytes = decodeHex(argv[1]);
+  char *base64 = encodeBase64(rawBytes);
+
+  printf("%s\n", base64);
+
+  free(rawBytes);
+  free(base64);
 
   return EXIT_SUCCESS;
 }
