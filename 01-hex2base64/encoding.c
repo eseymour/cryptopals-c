6b26ab0355961f52638664e85e101568266da980
Edward Seymour <dev@edwardseymour.me>
2017-10-09 22:42:50 -0600

    Implement decodeHex

--- 01-hex2base64/encoding.c
+++ 01-hex2base64/encoding.c
@@ -1,13 +1,26 @@
 #include "encoding.h"
 
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define hex2nibble(x) (isdigit(x) ? x-'0' : isupper(x) ? x-'A'+10 : x-'a'+10)
 
 ByteString * decodeHex(char *encodedString) {
-  (void)encodedString;
-  fprintf(stderr, "%s not implemented yet.\n", __func__);
+  size_t numBytes = strlen(encodedString);
+  
+  ByteString *rawBytes = (ByteString *) malloc(sizeof(ByteString) + numBytes);
+  rawBytes->length = numBytes;
 
-  exit(EXIT_FAILURE);
+  for (size_t i = 0; i < numBytes; ++i) {
+    byte_t curByte = hex2nibble(encodedString[i*2]) << 4;
+    curByte |= hex2nibble(encodedString[i*2 + 1]);
+
+    rawBytes->bytes[i] = curByte;
+  }
+
+  return rawBytes;
 }
 
 char * encodeBase64(ByteString *rawBytes) {
