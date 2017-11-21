072b06fb91952f6cc54b78ce4d2f3ea8c76811d4
Edward Seymour <dev@edwardseymour.me>
2017-11-20 20:43:53 -0700

    Add error message to failing test

--- 01-hex2base64/hex2base64_test.sh
+++ 01-hex2base64/hex2base64_test.sh
@@ -3,4 +3,7 @@
 input=49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
 expected=SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
 
-diff <(./hex2base64 $input) <(echo $expected)
+if [[ $(diff <(./hex2base64 $input) <(echo $expected)) ]]; then
+  echo "ERROR: Program output different base64 string than expected"
+  exit 1
+fi
