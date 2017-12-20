31d56d3e7b9c28fb01e168e27a48ca251b1a33a1
Edward Seymour <dev@edwardseymour.me>
2017-12-19 22:54:54 -0700

    Finish Fixed XOR challenge

--- 02-fixedxor/fixedxor.c
+++ 02-fixedxor/fixedxor.c
@@ -4,6 +4,8 @@
 
 #include <basecodec/basecodec.h>
 
+ByteString * fixedXorEncode(ByteString *rawMessage, ByteString *rawKey);
+
 int main(int argc, char **argv) {
   if (argc != 3) {
     fprintf(stderr, "usage: %s hex_message hex_key\n", strrchr(argv[0], '/') + 1);
@@ -15,5 +17,30 @@ int main(int argc, char **argv) {
     return EXIT_FAILURE;
   }
 
+  ByteString *rawMessage = decodeHex(argv[1]);
+  ByteString *rawKey = decodeHex(argv[2]);
+  ByteString *rawEncodedMessage = fixedXorEncode(rawMessage, rawKey);
+
+  char *encodedMessage = encodeHex(rawEncodedMessage);
+  printf("%s\n", encodedMessage);
+
+  free(rawMessage);
+  free(rawKey);
+  free(rawEncodedMessage);
+  free(encodedMessage);
+
   return EXIT_SUCCESS;
+}
+
+ByteString * fixedXorEncode(ByteString *rawMessage, ByteString *rawKey) {
+  size_t messageLen = rawMessage->length;
+
+  ByteString *encodedMessage = (ByteString *) malloc(sizeof(ByteString) + messageLen);
+  encodedMessage->length = messageLen;
+
+  for (size_t i = 0; i < messageLen; ++i) {
+    encodedMessage->bytes[i] = rawMessage->bytes[i] ^ rawKey->bytes[i];
+  }
+
+  return encodedMessage;
 }
