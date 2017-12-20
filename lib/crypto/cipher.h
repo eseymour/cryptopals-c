54ea2225712d63336ced2bde12219982b19dcf15
Edward Seymour <dev@edwardseymour.me>
2017-12-20 15:24:32 -0700

    Add repeatingKey and singleByte XorCiphers

--- lib/crypto/cipher.h
+++ lib/crypto/cipher.h
@@ -4,9 +4,24 @@
 #include <stddef.h>
 #include <stdint.h>
 
+void repeatingKeyXorCipher(
+  uint8_t *message,
+  uint8_t *key,
+  uint8_t *cipherText,
+  size_t messageSize,
+  size_t keySize
+);
+
 void fixedXorCipher(
   uint8_t *message,
   uint8_t *key,
+  uint8_t *ciphertext,
+  size_t messageSize
+);
+
+void singleByteXorCipher(
+  uint8_t *message,
+  uint8_t key,
   uint8_t *ciphertext,
   size_t messageSize
 );
--- lib/crypto/xorcipher.c
+++ lib/crypto/xorcipher.c
@@ -1,12 +1,31 @@
 #include "cipher.h"
 
+void repeatingKeyXorCipher(
+  uint8_t *message,
+  uint8_t *key,
+  uint8_t *cipherText,
+  size_t messageSize,
+  size_t keySize
+) {
+  for (size_t i = 0; i < messageSize; i++) {
+    cipherText[i] = message[i] ^ key[i % keySize];
+  }
+}
+
 void fixedXorCipher(
   uint8_t *message,
   uint8_t *key,
   uint8_t *ciphertext,
   size_t messageSize
 ) {
-  for (size_t i = 0; i < messageSize; ++i) {
-    ciphertext[i] = message[i] ^ key[i];
-  }
+  repeatingKeyXorCipher(message, key, ciphertext, messageSize, messageSize);
+}
+
+void singleByteXorCipher(
+  uint8_t *message,
+  uint8_t key,
+  uint8_t *ciphertext,
+  size_t messageSize
+) {
+  repeatingKeyXorCipher(message, &key, ciphertext, messageSize, 1);
 }
