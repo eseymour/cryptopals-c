31b6b6c41de3e7f8a31d4a781f561f8d4ad32e6e
Edward Seymour <dev@edwardseymour.me>
2017-12-19 22:18:47 -0700

    Create high level logic for fixedxor

--- 02-fixedxor/fixedxor.c
+++ 02-fixedxor/fixedxor.c
@@ -1,3 +1,19 @@
-int main(void) {
-  return 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <basecodec/basecodec.h>
+
+int main(int argc, char **argv) {
+  if (argc != 3) {
+    fprintf(stderr, "usage: %s hex_message hex_key\n", strrchr(argv[0], '/') + 1);
+    return EXIT_FAILURE;
+  }
+
+  if (strlen(argv[1]) != strlen(argv[2])) {
+    fprintf(stderr, "key and message lengths do not match\n");
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
 }
